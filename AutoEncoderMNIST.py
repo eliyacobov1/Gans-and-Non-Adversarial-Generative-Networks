b020798f6fe12dd97a7cf43a83615200a8784240
Eli Yacobov <eli.yacobov1@gmail.com>
2021-06-06 22:27:34 +0300

    .

--- AutoEncoderMNIST.py
+++ AutoEncoderMNIST.py
@@ -86,6 +86,15 @@ def calc_kurtosis(t, mean, std):
     return torch.mean(((t - mean) / std) ** 4)
 
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
 def train(net: AutoEncoderMNIST, dataloader, criterion=nn.MSELoss()):
     optimizer = optim.Adam(net.parameters(), lr=lr, betas=(beta1, 0.999))
     print("Starting Training Loop...")
